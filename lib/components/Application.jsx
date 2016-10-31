2c7703031d2d7a8a3669114da80276b7ff9e3734
lrknaff <lacey.knaff@gmail.com>
2016-10-31 13:26:48 -0600

    add in sign in button, firebase google signin pop up enabled

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { pick, map, extend, filter} from 'lodash';
 import moment from 'moment';
+import firebase, { signIn } from '../firebase';
 // import firebase, { reference, signIn, signOut } from '../firebase';
 
 export default class Application extends Component {
@@ -9,6 +10,13 @@ export default class Application extends Component {
     return (
       <div>
         <h1>Hello World!</h1>
+      <section className='sign-in-out'>
+        <button
+          className='sign-in-button'
+          onClick={() => signIn()}>
+          Sign In
+        </button>
+      </section>
       </div>
     )
   }
