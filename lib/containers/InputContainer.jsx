f5771c394aad99f4b2c9753141f48ca75a28819f
lrknaff <lacey.knaff@gmail.com>
2016-11-03 10:25:41 -0600

    style of Forms and component cards

--- lib/containers/InputContainer.jsx
+++ lib/containers/InputContainer.jsx
@@ -17,23 +17,54 @@ export default class InputContainer extends Component {
 
     return (
       <form className='input-form'>
-        <InputComponent className='input-form-title' placeholder='Job Title' type='text' />
-        <InputComponent className='input-form-location' placeholder='Job Location' type='text' />
-        <InputComponent className='input-form-salary' placeholder='Salary' type='number' />
-        <InputComponent className='input-form-bonus' placeholder='Bonus' type='number' />
-        <InputComponent className='input-form-401k' placeholder='401k match %' type='number' />
-        <InputComponent className='input-form-insurance' placeholder='Insurance' type='number' />
-        <InputComponent className='input-form-distance' placeholder='Miles from Home' type='number' />
-        <form className='input-form-radio'>
-        <p>Lunch</p>
-          <InputComponent className='input-form-lunch' type='radio' /> Y
-          <InputComponent className='input-form-beer' type='radio' /> N
-        </form>
-        <form className='input-form-radio'>
-        <p>Beer/Wine</p>
-          <InputComponent className='input-form-lunch' type='radio' /> Y
-          <InputComponent className='input-form-beer' type='radio' /> N
-        </form>
+
+        <div className='input-form-container'>
+          <InputComponent className='input-form-title' type='text' />
+          <label>Company</label>
+        </div>
+
+        <div className='input-form-container'>
+          <InputComponent className='input-form-location' type='text' />
+          <label>Location</label>
+        </div>
+
+        <div className='input-form-container'>
+          <InputComponent className='input-form-salary' type='number' />
+          <label>Salary</label>
+        </div>
+
+        <div className='input-form-container'>
+          <InputComponent className='input-form-bonus' type='number' />
+          <label>Bonus</label>
+        </div>
+
+        <div className='input-form-container'>
+          <InputComponent className='input-form-401k' type='number' />
+          <label>401K match %</label>
+        </div>
+
+        <div className='input-form-container'>
+          <InputComponent className='input-form-insurance' type='number' />
+          <label>Insurance</label>
+        </div>
+
+        <div className='input-form-container'>
+          <InputComponent className='input-form-distance' type='number' />
+          <label>Distance from home</label>
+        </div>
+
+          <form className='input-form-radio'>
+          <p>Lunch</p>
+            <InputComponent className='input-form-lunch' type='radio' /> Y
+            <InputComponent className='input-form-beer' type='radio' /> N
+          </form>
+
+          <form className='input-form-radio'>
+          <p>Beer/Wine</p>
+            <InputComponent className='input-form-lunch' type='radio' /> Y
+            <InputComponent className='input-form-beer' type='radio' /> N
+          </form>
+
       </form>
     )
   }
